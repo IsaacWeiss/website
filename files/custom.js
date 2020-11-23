d7211b3a23a7c45a12e5073ee232f98b8e42489c
IsaacWeiss <IsaacWeiss@users.noreply.github.com>
2020-11-23 16:00:04 -0500

    Use JS to animate menu opening/closing and disable hover for mobile

--- files/custom.js
+++ files/custom.js
@@ -1,85 +1,56 @@
-jQuery(function($) {
+jQuery(function ($) {
 
-    // Fixed nav
-    $.fn.checkHeaderPositioning = function(scrollEl, scrollClass) {
+  // sticky header helper function
+  $.fn.checkHeaderPositioning = function (scrollEl, scrollClass) {
 
-        if (!this.length) {
-          return;
-        }
+    if (!this.length) {
+      return;
+    }
 
-        if($(scrollEl).scrollTop() > 50) {
-            this.addClass(scrollClass);
-        } else if($(scrollEl).scrollTop() === 0) {
-            this.removeClass(scrollClass);
-        }
-    };
-
-  // Interval loop
-  $.fn.intervalLoop = function(condition, action, duration, limit) {
-    var counter = 0;
-    var looper = setInterval(function(){
-      if (counter >= limit || $.fn.checkIfElementExists(condition)) {
-        clearInterval(looper);
-      } else {
-        action();
-        counter++;
-      }
-    }, duration);
-  }
-
-  // Check if element exists
-  $.fn.checkIfElementExists = function(selector) {
-    return $(selector).length;
-  }
+    if ($(scrollEl).scrollTop() > 50) {
+      this.addClass(scrollClass);
+    } else if ($(scrollEl).scrollTop() === 0) {
+      this.removeClass(scrollClass);
+    }
+  };
 
   var controller = {
-    init: function(opts) {
+    init: function () {
       var base = this;
 
       $('body').checkHeaderPositioning(window, 'affix');
 
-      // Add classes to elements
-      base._addClasses();
-
-      setTimeout(function(){
+      setTimeout(function () {
         base._attachEvents();
       }, 1000);
     },
 
-    _addClasses: function() {
-      var base = this;
-    },
+    _attachEvents: function () {
 
-    _attachEvents: function() {
-      var base = this;
+      // mobile nav toggle
+      $('label.hamburger').on('click', function () {
+        $("nav").slideToggle("fast");
+        if (!$('body').hasClass('nav-open')) {
+          $('body').addClass('nav-open');
+        } else {
+          $('body').removeClass('nav-open');
+        }
+      });
 
-        $('label.hamburger').on('click', function() {
-            if (!$('body').hasClass('nav-open')) {
-                $('body').addClass('nav-open');
-            } else {
-                $('body').removeClass('nav-open');
-            }
-        });
+      // mobile subnav toggle
+      $('li.has-submenu').on('click', function () {
+        $(".submenu").slideToggle("fast");
+      });
 
-        // Fixed header
-        $(window).on('scroll', function() {
-          $('body').checkHeaderPositioning(window, 'affix');
-        });
+      // keep sticky header
+      $(window).on('scroll', function () {
+        $('body').checkHeaderPositioning(window, 'affix');
+      });
 
-        // Subnav toggle
-        $('li.has-submenu span.icon-caret').on('click', function() {
-            var $me = $(this);
-
-            if($me.siblings('.wsite-menu-wrap').hasClass('open')) {
-                $me.siblings('.wsite-menu-wrap').removeClass('open');
-            } else {
-                $me.siblings('.wsite-menu-wrap').addClass('open');
-            }
-        });
-    },
+    }
   }
 
-  $(document).ready(function(){
+  $(document).ready(function () {
     controller.init();
   });
 });
